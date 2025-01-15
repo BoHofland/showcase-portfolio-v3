b6a17e342cc6534fb9d2835bfd14a0ab4eed0631
BoHofland <bo.hofland@student.fontys.nl>
2025-01-15 19:38:35 +0100

    over mij animatie veranderd
    van bewegende animatie, naar vaste tekst met veranderende kleuren

--- showcase-portfolio-v3/app/about/page.jsx
+++ showcase-portfolio-v3/app/about/page.jsx
@@ -1,20 +1,39 @@
+"use client";
+import { useState, useEffect } from 'react';
 import Header from '../components/header/page';
 import Contact from '../components/contact/page';
-import TextSliderOverMij from '../components/TextSliderOver-mij/TextSliderOver-mij';
+
 export default function About() {
+  const colors = [
+    'text-blue-500',
+    'text-orange-500',
+    'text-yellow-500',
+    'text-purple-500'
+  ];
+
+  const [colorIndex, setColorIndex] = useState(0);
+
+  useEffect(() => {
+    const colorInterval = setInterval(() => {
+      setColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
+    }, 5000); // Verandert elke 5 seconden van kleur
+
+    return () => clearInterval(colorInterval);
+  }, []);
+
   return (
     <main className="min-h-screen bg-background text-white">
       <Header />
 
-      <div className="py-32 md:py-40">
-        <TextSliderOverMij />
-      </div>
-      
       <div className="container mx-auto px-4 py-16 mt-16">
+        {/* Over mij sectie */}
+        <h1 className={`text-[12.5rem] font-bold mb-16 mt-16 transition-colors duration-[2000ms] ${colors[colorIndex]}`}>
+          Over mij
+        </h1>
 
 
 
-        <div className="text-2xl text-gray-300 space-y-8 max-w-4xl mb-32">
+        <div className="text-2xl text-gray-300 space-y-8 max-w-4xl mb-48">
           <p>
             Hoi, ik ben Bo.
           </p>
@@ -41,12 +60,15 @@ export default function About() {
           </p>
         </div>
 
-        <h2 className="text-8xl font-bold mb-8">
-          Skills
-        </h2>
-        <p className="text-2xl text-gray-300">
-          UX/UI designer / Front-End developer
-        </p>
+        {/* Skills sectie */}
+        <div className="mt-32">
+          <h2 className="text-8xl font-bold mb-8">
+            Skills
+          </h2>
+          <p className="text-2xl text-gray-300">
+            UX/UI designer / Front-End developer
+          </p>
+        </div>
       </div>
       <Contact />
     </main>
